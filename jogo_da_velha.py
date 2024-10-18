aaf11fa63ec56d02de51d8247bb376373a2643ac
arthur <arthur@gmail.com>
2024-10-17 22:31:34 -0300

    tabuleiro

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -6,15 +6,15 @@ import pygame #importa a biblioteca pygame para o script
 pygame.init() #inicialização do pygame
 pygame.font.init() #inicialização do pacote de fontes no pygame
 
-screen = pygame.display.set_mode((500, 500)) #definição do tamanho da tela
+screen = pygame.display.set_mode((600, 600)) #definição do tamanho da tela
 pygame.display.set_caption('Jogo da Velha') #nome da janela do jogo
 clock = pygame.time.Clock() #biblioteca de tempo
 
-fonte_quadrinhos = pygame.font.SysFont('Comic Sans Ms', 100, True, True) #importar fonte
+fonte_quadrinhos = pygame.font.SysFont('Comic Sans MS', 100, True, True) #importar fonte
 running = True #variável de controle do status do jogo
 
-personagem_x = fonte_quadrinhos.render('X', True, 'red')
-personagem_y = fonte_quadrinhos.render('O', True, 'red')
+personagem_x = fonte_quadrinhos.render('X', True, '#87CEFA')
+personagem_y = fonte_quadrinhos.render('O', True, '#FF69B4')
 cor_fundo = 1
 
 while running:
@@ -30,14 +30,29 @@ while running:
             if(cor_fundo > 3):
                 cor_fundo = 1
  
-    if cor_fundo == 1:
-        screen.fill('black')
-        screen.blit(personagem_x,(250,250))
-    elif cor_fundo == 2:
-        screen.fill('black')
-        screen.blit(personagem_y,(250,250))
-    else:
-        screen.fill('purple')
+ #tabuleiro
+    screen.fill('#F0F8FF')
+    pygame.draw.line(screen, '#FFD700', (200, 20), (200, 580), 10)
+    pygame.draw.line(screen, '#FFD700', (400, 20), (400, 580), 10)
+    pygame.draw.line(screen, '#FFD700', (20, 200), (580, 200), 10)
+    pygame.draw.line(screen, '#FFD700', (20, 400), (580, 400), 10)
+
+    # if cor_fundo == 1:
+    #    screen.blit(personagem_x,(60, 30))
+    # # elif cor_fundo == 2:
+    #     screen.blit(personagem_y,(260,30))
+    #     screen.blit(personagem_x,(60, 30))
+    # # elif cor_fundo == 3:
+    #     screen.blit(personagem_y,(260,30))
+    #     screen.blit(personagem_x,(60, 30))
+    #     screen.blit(personagem_x,(460,230))   
+    # # elif cor_fundo == 4:
+    screen.blit(personagem_y,(260,30))
+    screen.blit(personagem_x,(60, 30))
+    screen.blit(personagem_x,(460,230))   
+    screen.blit(personagem_y,(260,230))
+    screen.blit(personagem_y,(260,430))
+
 
 
     # flip() o display para atualizar a página
